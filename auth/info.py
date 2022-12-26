17a0f46d142c429303cf835df797283fd533882b
JameeBashir1 <91050731+JameeBashir1@users.noreply.github.com>
2022-12-26 15:50:44 +0530

    Delete auth directory

--- auth/info.py
+++
@@ -1,7 +0,0 @@
-EMAIL_USE_TLS =True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'iotmajorproject4@gmail.com'
-EMAIL_HOST_PASSWORD = 'yltvntaevigvlezl'
-EMAIL_PORT = 587
-SPECIAL_KEY = 'dummy@123'
-CB= 'checked'
